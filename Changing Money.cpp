1c4e6d91d0a8a2166c4c068c5861f1768eaaf2b5
Vishnu S Kumar <vishnusk1995@gmail.com>
2016-05-18 07:28:51 -0400

    Update Changing Money.cpp

--- Changing Money.cpp
+++ Changing Money.cpp
@@ -1,24 +1,32 @@
-// Changing Money.cpp : Defines the entry point for the console application.
+// Changing Money.cpp : Defines the entry poLong for the console application.
 /*
 The goal in this problem is to find the minimum number of coins needed to change the given amount
 of money using coins with denominations 1, 5, and 10.
 */
 
 #include <iostream>
+typedef unsigned long long Long;
 
-int getCoins(int amount) {
-
-	return amount % 10;
+Long getCoins(Long amount) {
+	int coins[] = { 10,5,1 };
+	Long count = 0;
+	for (int coin : coins) {
+		while (amount >= coin) {
+			++count;
+			amount -= coin;
+		}
+	}
+	return count;
 }
 
 int main()
 {
-	int amt = 0;
+	Long amt = 0;
 	while (std::cin.good())
 	{
 		std::cout << "How Much Amount : "; std::cin >> amt;
 		std::cout << "Minimum Number of Coins with denominations 1, 5, and 10 = " << getCoins(amt) << "\n\n";
 	}
-    return 0;
+	return 0;
 }
 
